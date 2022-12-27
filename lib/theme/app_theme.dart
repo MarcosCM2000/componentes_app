f2e130311d148e881efb0e45c38e434b34079f1a
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-12-27 11:05:31 -0600

    centralizar widgets

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,7 +4,9 @@ class AppTheme {
   static const Color primary = Colors.green;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
       primaryColor: primary,
-      appBarTheme: const AppBarTheme(color: primary, elevation: 0));
+      appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+      textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(foregroundColor: primary)));
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
       primaryColor: primary,
       appBarTheme: const AppBarTheme(color: primary, elevation: 0),
