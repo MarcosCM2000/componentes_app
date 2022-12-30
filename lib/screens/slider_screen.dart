cf62695da5a7dda3a8b55687d4d5f8a21f7395e4
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-12-30 11:26:16 -0600

    checkbox & switches

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -11,6 +11,7 @@ class SliderScreen extends StatefulWidget {
 
 class _SliderScreenState extends State<SliderScreen> {
   double _sliderValue = 100;
+  bool _sliderEnabled = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,16 +26,43 @@ class _SliderScreenState extends State<SliderScreen> {
             value: _sliderValue,
             activeColor: AppTheme.primary,
             divisions: 5,
+            onChanged: _sliderEnabled
+                ? ((value) {
+                    _sliderValue = value;
+                    setState(() {});
+                  })
+                : null),
+        Checkbox(
+            value: _sliderEnabled,
             onChanged: ((value) {
-              _sliderValue = value;
+              _sliderEnabled = value ?? true;
               setState(() {});
             })),
-        SingleChildScrollView(
-          child: Image(
-            image: const NetworkImage(
-                'https://www.dc.com/sites/default/files/Char_Gallery_Flash_758_6055049612af35.61135649.jpg'),
-            fit: BoxFit.contain,
-            width: _sliderValue,
+        CheckboxListTile(
+            title: const Text('Habilitar slider'),
+            activeColor: AppTheme.primary,
+            value: _sliderEnabled,
+            onChanged: ((value) {
+              _sliderEnabled = value ?? true;
+              setState(() {});
+            })),
+        SwitchListTile.adaptive(
+            activeColor: AppTheme.primary,
+            title: const Text('Habilitar slider'),
+            value: _sliderEnabled,
+            onChanged: ((value) {
+              _sliderEnabled = value;
+              setState(() {});
+            })),
+        const AboutListTile(),
+        Expanded(
+          child: SingleChildScrollView(
+            child: Image(
+              image: const NetworkImage(
+                  'https://www.dc.com/sites/default/files/Char_Gallery_Flash_758_6055049612af35.61135649.jpg'),
+              fit: BoxFit.contain,
+              width: _sliderValue,
+            ),
           ),
         ),
       ]),
